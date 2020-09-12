f37cdfd380e2afaad3d53403cfda99ce39cff5f0
hakolao <okkohakola@gmail.com>
2020-09-12 15:32:34 +0300

    Init

---
+++ tests/test_state.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_state.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/16 19:58:24 by ohakola           #+#    #+#             */
+/*   Updated: 2020/09/12 15:23:31 by ohakola          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mu_test.h"
+
+t_test_state	test_state_create(t_bool success, const char *result, int id)
+{
+	t_test_state	state;
+
+	state = (t_test_state){.success = success, .result = result, .id = id};
+	return (state);
+}

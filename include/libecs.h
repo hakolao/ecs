01c1e725ca9625e29a2f848658a7b901a9e63087
Okko Hakola <ohakola@c2r6p2.hive.fi>
2020-09-18 16:15:30 +0300

    Add some comments

--- include/libecs.h
+++ include/libecs.h
@@ -6,7 +6,7 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/17 13:11:01 by ohakola           #+#    #+#             */
-/*   Updated: 2020/09/18 14:02:24 by ohakola          ###   ########.fr       */
+/*   Updated: 2020/09/18 16:15:13 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@
 
 /*
 ** 64 (max number of bits in uint64_t (for bitmasks)).
+** Entities are like 11110001111, where each bit corresponds to one component.
+** Thus a component should be defined like 1 << 3 as an enum. See demo example
+** or tests.
 */
 
 # define ECS_MAX_COMPONENTS 64
@@ -33,7 +36,7 @@
 ** Some util macros
 */
 
-# define ECS_SYSTEM_EMPTY UINT64_MAX
+# define ECS_SYSTEM_EMPTY UINT32_MAX
 # define ECS_NO_COMPONENT 0
 # define ECS_EMPTY_ENTITY 0
 
@@ -55,6 +58,12 @@ typedef struct				s_system
 	void				*params;
 }							t_system;
 
+/*
+** Component data struct to hold arbitrary data that gets passed to hash maps
+** which return component by entity index. Size is necessary so we know
+** how much to allocate and copy for entity.
+*/
+
 typedef struct				s_component
 {
 	uint64_t			id;
@@ -66,6 +75,17 @@ typedef struct				s_component
 ** World is the core of the ECS library, it contains entities, and maps
 ** relations between entities and components they hold. It also contains
 ** a reference to all possible components that entities have.
+** @entities: Enity masks (set of components) per entity
+** @vacant_entities: Holds knowledge of removed entities and those indices
+** are filled first when adding new entities.
+** @components_by_entity: A list of hash maps per each component holding
+** component data by each entity index.
+** @index_by_component: Hash map containing an index per component to access
+** components_by_entity faster.
+** @systems: Holds system data and info on components belonging to that system
+** this is used to run system handles on each entity that matches the components
+** @index_by_system: A hash map holding the indices by system id to faster
+** access systems.
 */
 
 struct						s_ecs_world
@@ -88,7 +108,7 @@ struct						s_ecs_world
 };
 
 /*
-** World init & destroy
+** World creation & destroy
 */
 
 t_ecs_world					*ecs_world_create(const char *name,
@@ -96,7 +116,7 @@ t_ecs_world					*ecs_world_create(const char *name,
 void						ecs_world_destroy(t_ecs_world *world);
 
 /*
-** Components
+** Components related to world data
 */
 
 void						ecs_world_component_add(t_ecs_world *world,
@@ -109,7 +129,7 @@ t_hash_table				*ecs_component_entities(t_ecs_world *world,
 							uint64_t component_id);
 
 /*
-** Entities
+** Entities & entity management
 */
 
 int64_t						ecs_world_entity_add(t_ecs_world *world,
@@ -134,7 +154,7 @@ void						*ecs_world_entity_component_get(t_ecs_world *world,
 							uint64_t entity_index, uint64_t component);
 
 /*
-** Systems
+** Systems and system management
 */
 
 void						ecs_world_system_add(t_ecs_world *world,
@@ -149,9 +169,5 @@ void						ecs_system_update_params(t_ecs_world *world,
 							uint64_t system_id, void *params);
 void						ecs_systems_run_single(t_ecs_world *world,
 							uint64_t system_id);
-
-/*
-** ECS utils
-*/
 
 #endif

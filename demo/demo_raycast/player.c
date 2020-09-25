a01d30b9e97bd1b1ca6a0c6b12a5625c95524455
hakolao <okkohakola@gmail.com>
2020-09-25 18:30:42 +0300

    Save in a crappy state

--- demo/demo_raycast/player.c
+++ demo/demo_raycast/player.c
@@ -6,35 +6,11 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/24 14:09:55 by ohakola           #+#    #+#             */
-/*   Updated: 2020/09/25 17:58:15 by ohakola          ###   ########.fr       */
+/*   Updated: 2020/09/25 18:28:17 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "demo_raycast.h"
-
-static void		apply_translate_to_world(t_demo_data *data, t_mat4 translate)
-{
-	int		i;
-
-	i = -1;
-	while (++i < (int)data->num_objects)
-	{
-		transform_3d_object(data->objects[i], translate, trans_translate);
-		update_3d_object_kd_tree(data->objects[i]);
-	}
-}
-
-static void		apply_rotation_to_world(t_demo_data *data, t_mat4 rotation)
-{
-	int		i;
-
-	i = -1;
-	while (++i < (int)data->num_objects)
-	{
-		transform_3d_object(data->objects[i], rotation, trans_rotate);
-		update_3d_object_kd_tree(data->objects[i]);
-	}
-}
 
 void			rotate_player(t_demo_data *data, uint32_t delta_time, t_vec3 axes)
 {
@@ -45,7 +21,6 @@ void			rotate_player(t_demo_data *data, uint32_t delta_time, t_vec3 axes)
 	ml_matrix4_mul_vec3(rotation, data->player_forward, data->player_forward);
 	ml_matrix4_mul_vec3(rotation, data->player_up, data->player_up);
 	ml_matrix4_mul_vec3(rotation, data->player_sideways, data->player_sideways);
-	apply_rotation_to_world(data, rotation);
 }
 
 void			move_player(t_demo_data *data, uint32_t delta_time, t_move dir)
@@ -74,7 +49,6 @@ void			move_player(t_demo_data *data, uint32_t delta_time, t_move dir)
 		ml_vector3_mul(data->player_sideways, data->player_speed * delta_time, add);
 		ml_vector3_sub(data->player_pos, data->player_sideways, new_pos);
 	}
-	ml_matrix4_translation(-add[0], -add[1], -add[2], world_translation);
-	apply_translate_to_world(data, world_translation);
 	ml_vector3_copy(new_pos, data->player_pos);
+	ml_matrix4_translation(-add[0], -add[1], -add[2], world_translation);
 }
--- demo/demo_raycast/scene.c
+++ demo/demo_raycast/scene.c
@@ -6,11 +6,25 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/24 12:06:53 by ohakola           #+#    #+#             */
-/*   Updated: 2020/09/25 17:52:28 by ohakola          ###   ########.fr       */
+/*   Updated: 2020/09/25 18:27:16 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "demo_raycast.h"
+
+void			apply_transforms_to_world(t_demo_data *data)
+{
+	int		i;
+
+	i = -1;
+	while (++i < (int)data->num_objects)
+	{
+		transform_3d_object(data->objects[i], data->world_scale, trans_scale);
+		transform_3d_object(data->objects[i], data->world_rotation, trans_rotate);
+		transform_3d_object(data->objects[i], data->world_translation, trans_translate);
+		update_3d_object_kd_tree(data->objects[i]);
+	}
+}
 
 static void		init_scene_player(t_demo_data *data)
 {
@@ -29,8 +43,6 @@ static void		init_scene_player(t_demo_data *data)
 void			demo_scene_create(t_app *app)
 {
 	t_demo_data *data;
-	t_mat4		translate;
-	t_mat4		scale;
 	uint32_t	i;
 
 	data = (t_demo_data*)app->data;
@@ -40,21 +52,16 @@ void			demo_scene_create(t_app *app)
 		data->objects[i] = NULL;
 	data->objects[0] = read_object_file(ICOSPHERE_PATH);
 	data->objects[1] = read_object_file(ICOSPHERE_PATH);
-	data->num_objects = 2;
+	data->num_objects = 1;
 	ml_vector3_copy((t_vec3){0, 0, 0}, data->camera_pos);
 	data->fov = 90.0;
-	ml_matrix4_id(scale);
-	scale[0][0] = 30;
-	scale[1][1] = 30;
-	scale[2][2] = 30;
-	ml_matrix4_translation(0, 0, -200, translate);
-	transform_3d_object(data->objects[0], scale, trans_scale);
-	transform_3d_object(data->objects[0], translate, trans_translate);
-	ml_matrix4_translation(-50, 0, -200, translate);
-	transform_3d_object(data->objects[1], scale, trans_scale);
-	transform_3d_object(data->objects[1], translate, trans_translate);
-	update_3d_object_kd_tree(data->objects[0]);
-	update_3d_object_kd_tree(data->objects[1]);
+	ml_matrix4_id(data->world_scale);
+	ml_matrix4_id(data->world_rotation);
+	data->world_scale[0][0] = 30;
+	data->world_scale[1][1] = 30;
+	data->world_scale[2][2] = 30;
+	ml_matrix4_translation(0, 0, -200, data->world_translation);
+	apply_transforms_to_world(data);
 }
 
 void			demo_scene_destroy(t_app *app)

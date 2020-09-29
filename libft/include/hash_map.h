2fcf34228a9ff8dff6c8fe1cc45efe95f2fc0568
hakolao <okkohakola@gmail.com>
2020-09-30 01:30:38 +0300

    normify libft

--- libft/include/hash_map.h
+++ libft/include/hash_map.h
@@ -6,7 +6,7 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/12 17:27:15 by ohakola           #+#    #+#             */
-/*   Updated: 2020/09/17 20:36:50 by ohakola          ###   ########.fr       */
+/*   Updated: 2020/09/30 01:25:46 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,20 @@
 # include <stdlib.h>
 # include "t_bool.h"
 
-typedef struct	s_hash_node t_hash_node;
+typedef struct s_hash_node	t_hash_node;
 
-struct			s_hash_node
+struct						s_hash_node
 {
 	int			key;
 	void		*val;
 	t_hash_node	*next;
 };
 
-typedef struct	s_hash_table
+typedef struct				s_hash_table
 {
 	int			size;
 	t_hash_node	**list;
-}				t_hash_table;
+}							t_hash_table;
 
 /*
 ** Hash map / Dictionary / Hash table (whatever you want to call it).
@@ -48,17 +48,19 @@ typedef struct	s_hash_table
 ** (if malloced)
 */
 
-void			hash_map_delete(t_hash_table *table, int key);
-void			hash_map_delete_free(t_hash_table *table, int key);
-void			hash_map_clear(t_hash_table *table);
-void			hash_map_clear_free(t_hash_table *table);
-void			hash_map_destroy(t_hash_table *table);
-void			hash_map_destroy_free(t_hash_table *table);
-t_hash_table	*hash_map_create(int size);
-int				hash_map_hash(t_hash_table *table, int key);
-void			*hash_map_get(t_hash_table *table, int key);
-void			hash_map_add(t_hash_table *table, int key, void *val);
-t_bool			hash_map_has_key(t_hash_table *table, int key);
-void			hash_map_foreach(t_hash_table *table, void (*f)(void *val));
+void						hash_map_delete(t_hash_table *table, int key);
+void						hash_map_delete_free(t_hash_table *table, int key);
+void						hash_map_clear(t_hash_table *table);
+void						hash_map_clear_free(t_hash_table *table);
+void						hash_map_destroy(t_hash_table *table);
+void						hash_map_destroy_free(t_hash_table *table);
+t_hash_table				*hash_map_create(int size);
+int							hash_map_hash(t_hash_table *table, int key);
+void						*hash_map_get(t_hash_table *table, int key);
+void						hash_map_add(t_hash_table *table, int key,
+							void *val);
+t_bool						hash_map_has_key(t_hash_table *table, int key);
+void						hash_map_foreach(t_hash_table *table,
+							void (*f)(void *val));
 
 #endif
